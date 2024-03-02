0ac54b7c5e5d3e744b4bd87ac6faf7cbca87b0d9
aort <artashesano@gmail.com>
2024-03-02 17:14:47 +0400

    Display remaining milliseconds on video player timer

--- src/utils/getTimeFromSeconds.ts
+++ src/utils/getTimeFromSeconds.ts
@@ -1,8 +1,12 @@
 function getTimeFromSeconds(seconds: number) {
-  seconds = Math.max(0, seconds);
-
   const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = Math.floor(seconds % 60);
+  let remainingSeconds = (seconds % 60).toFixed(1);
+
+  if (String(remainingSeconds).split(".")[1] !== "0") {
+    remainingSeconds = String(remainingSeconds).padStart(4, "0");
+  } else {
+    remainingSeconds = Number(remainingSeconds).toFixed(0);
+  }
 
   const formattedMinutes = String(minutes).padStart(2, "0");
   const formattedSeconds = String(remainingSeconds).padStart(2, "0");
