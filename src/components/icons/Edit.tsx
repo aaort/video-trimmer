4d529f08578cc0ad764f6733e277dda0436dd42a
aort <artashesano@gmail.com>
2024-03-03 17:40:08 +0400

    Add Edit icon

---
+++ src/components/icons/Edit.tsx
@@ -0,0 +1,20 @@
+function Edit() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="var(--primary-color)"
+      stroke="var(--primary-color)"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+      <path d="m15 5 4 4" />
+    </svg>
+  );
+}
+
+export default Edit;
